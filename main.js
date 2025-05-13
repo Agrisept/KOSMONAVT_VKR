e830a80e3f550032e14e466bdfe0264e68d370d8
Agrisept <Agriseptl2015@gmail.com>
2025-05-13 22:27:45 +0300

    Main_list + Info_list

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 var timeElement = document.getElementById('currentTime');
-        function updateTime() {
-            var currentTime = new Date();
-            timeElement.textContent = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-        }
-        setInterval(updateTime, 1000);
-        updateTime();
+    function updateTime() {
+        var currentTime = new Date();
+        timeElement.textContent = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    }
+    setInterval(updateTime, 1000);
+    updateTime();
+
+ymaps.ready(init);
+    function init() {
+      var myMap = new ymaps.Map("map", {
+        center: [59.539611, 30.881228],
+        zoom: 16,
+        controls: ['zoomControl']
+      });
+
+      myMap.setType('yandex#map');
+      myMap.panes.get('ground').getElement().style.filter = 'grayscale(100%)    
+      var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        preset: 'islands#violetDotIcon'
+      } 
+      myMap.geoObjects.add(myPlacemark);
+    }

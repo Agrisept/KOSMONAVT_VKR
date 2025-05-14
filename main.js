9de9c206f7efe59213ef7e241663dfd01c976381
Agrisept <Agriseptl2015@gmail.com>
2025-05-14 18:45:03 +0300

    Mufasa_film_02 (seats)

--- main.js
+++ main.js
@@ -7,17 +7,35 @@ var timeElement = document.getElementById('currentTime');
     updateTime();
 
 ymaps.ready(init);
-    function init() {
-      var myMap = new ymaps.Map("map", {
-        center: [59.539611, 30.881228],
-        zoom: 16,
-        controls: ['zoomControl']
-      });
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [59.539611, 30.881228],
+      zoom: 16,
+      controls: ['zoomControl']
+    });
+    myMap.setType('yandex#map');
+    myMap.panes.get('ground').getElement().style.filter = 'grayscale(100%)'
+    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+      preset: 'islands#violetDotIcon'
+    } 
+    myMap.geoObjects.add(myPlacemark);
+  }
 
-      myMap.setType('yandex#map');
-      myMap.panes.get('ground').getElement().style.filter = 'grayscale(100%)'
-      var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
-        preset: 'islands#violetDotIcon'
-      } 
-      myMap.geoObjects.add(myPlacemark);
-    }
+document.addEventListener('DOMContentLoaded', function() {
+document.querySelector('.btn:nth-of-type(1)').addEventListener('click', function() {
+  alert('Открывается трейлер фильма');
+});
+  
+document.querySelector('.btn:nth-of-type(2)').addEventListener('click', function() {
+  alert('Переход к покупке билета');
+});
+
+const timeSlots = document.querySelectorAll('.time-slot');
+timeSlots.forEach(slot => {
+  slot.addEventListener('click', function() {
+    timeSlots.forEach(s => s.classList.remove('active'));
+    this.classList.add('active');
+    alert('Выбрано время: ' + this.textContent);
+  });
+});
+});
